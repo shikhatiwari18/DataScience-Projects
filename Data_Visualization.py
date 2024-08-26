5c5f2bd6245d135109b09bd39c31a65a44c8b666
shikhatiwari18 <52368541+shikhatiwari18@users.noreply.github.com>
2024-08-26 12:03:20 +0530

    Add files via upload

---
+++ Data_Visualization.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+iris = pd.read_csv("C:\\Users\\Shikha_Tiwari\\OneDrive - Dell Technologies\\Desktop\\\AIML-simplilearn\\Chapter3-DatascienceForPython\\Iris.csv")
+print(iris)
+#"C:\Users\Shikha_Tiwari\OneDrive - Dell Technologies\Desktop\AIML-simplilearn\Chapter3-DatascienceForPython\Iris.csv"
